0c92fc6a7c1c05e98b1ffad771fa37523641ca48
ArtyomKr <ArtyomKr@users.noreply.github.com>
2020-04-05 17:09:36 +0300

    Updates

---
+++ precache-manifest.f5f37a20bcd14fe8d73e23c1ecaa6c32.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039ba0073240a1eaf99101b3ff7f6f6d",
+    "url": "/hospital-app/index.html"
+  },
+  {
+    "revision": "19303b820aa4a79c567c",
+    "url": "/hospital-app/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e99c52a92bf9735a1ac3",
+    "url": "/hospital-app/static/css/main.2088cdb4.chunk.css"
+  },
+  {
+    "revision": "19303b820aa4a79c567c",
+    "url": "/hospital-app/static/js/2.b3037932.chunk.js"
+  },
+  {
+    "revision": "ec1f85f1bca9f81b1472297e40dc72a0",
+    "url": "/hospital-app/static/js/2.b3037932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99c52a92bf9735a1ac3",
+    "url": "/hospital-app/static/js/main.3ad8f45b.chunk.js"
+  },
+  {
+    "revision": "8ddc2c74d61cad27efb5",
+    "url": "/hospital-app/static/js/runtime-main.3b8fba49.js"
+  },
+  {
+    "revision": "9b4b0845efa011f46a370c31ce517d9f",
+    "url": "/hospital-app/static/media/Roboto-Light.9b4b0845.ttf"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/hospital-app/static/media/Roboto-Regular.90679e0c.ttf"
+  },
+  {
+    "revision": "6f191e6fb1511a7f8b27f33595014a6c",
+    "url": "/hospital-app/static/media/baby.6f191e6f.svg"
+  },
+  {
+    "revision": "d2b4ddebc1dc1b88bb0f35b536b5912e",
+    "url": "/hospital-app/static/media/calendar.d2b4ddeb.svg"
+  },
+  {
+    "revision": "ee0239d958db39eccc62fafe598db054",
+    "url": "/hospital-app/static/media/chat.ee0239d9.svg"
+  },
+  {
+    "revision": "41a9bb5c620676ad6c6cd51decfe01d3",
+    "url": "/hospital-app/static/media/doctor.41a9bb5c.svg"
+  },
+  {
+    "revision": "64d72b5d1fbde3060022f557e54c11d5",
+    "url": "/hospital-app/static/media/hospital.64d72b5d.svg"
+  },
+  {
+    "revision": "15f5ddb43ce1e65f2c349606451be9cf",
+    "url": "/hospital-app/static/media/medicine.15f5ddb4.svg"
+  },
+  {
+    "revision": "4e49cbb05caf70b7d9b87d2f8dc86dd7",
+    "url": "/hospital-app/static/media/speaker.4e49cbb0.svg"
+  },
+  {
+    "revision": "c566b5be39dc1cb3e7d3b94913aa2038",
+    "url": "/hospital-app/static/media/star.c566b5be.svg"
+  }
+]);
